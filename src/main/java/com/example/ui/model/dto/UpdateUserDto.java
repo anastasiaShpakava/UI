b3ce1aeb7af047815f3ef28d8cfedbced2f8208b
Anastasia Shpakova <shpakavaa@gmail.com>
2020-06-25 18:36:34 +0300

    create model

--- src/main/java/com/example/ui/model/dto/UpdateUserDto.java
+++ src/main/java/com/example/ui/model/dto/UpdateUserDto.java
@@ -16,7 +16,7 @@ import javax.validation.constraints.Size;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginUserDto {
+public class UpdateUserDto {
     private Integer id;
 
     @Size.List({
